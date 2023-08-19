eea776feafc3709d4345ea6c0156de54863802c4
Prince Gupta <guptasum521@gmail.com>
2023-08-20 00:05:10 +0530

    Create wp-mailify.php

---
+++ wp-mailify.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/princegup42
+ * @since             1.0.0
+ * @package           Wp_Mailify
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Mailify
+ * Plugin URI:        https://github.com/princegup42/wp-mailify
+ * Description:       Simplify your email sending process and enhance deliverability using WP Mailify's user-friendly interface
+ * Version:           1.0.0
+ * Author:            Sumit Kumar
+ * Author URI:        https://github.com/princegup42
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-mailify
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}

f9e57f5d71dc29e12e4eea8a52d4f63e1ead94d1
princegup42 <guptasum521@gmail.com>
2023-09-19 22:02:10 +0530

    Optimize and clean up admin page code

--- admin/partials/wp-mailify-admin-display.php
+++ admin/partials/wp-mailify-admin-display.php
@@ -14,37 +14,16 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<?php $this->wp_mailify_options = get_option( 'wp_mailify_option_name' ); ?>
+
 <div class="wrap">
     <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+    
     <form method="post" action="options.php">
-        <?php settings_fields($this->plugin_name); ?>
-        <?php do_settings_sections($this->plugin_name); ?>
-        <table class="form-table">
-            <tr valign="top">
-                <th scope="row"><?php esc_html_e('From Email Address', 'wp-mailify'); ?></th>
-                <td><input type="text" name="<?php echo $this->plugin_name; ?>_custom_email_from_address" value="<?php echo esc_attr(get_option('<?php echo $this->plugin_name; ?>_custom_email_from_address')); ?>" /></td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"><?php esc_html_e('From Name', 'wp-mailify'); ?></th>
-                <td><input type="text" name="<?php echo $this->plugin_name; ?>_custom_email_from_name" value="<?php echo esc_attr(get_option('<?php echo $this->plugin_name; ?>_custom_email_from_name')); ?>" /></td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"><?php esc_html_e('SMTP Host', 'wp-mailify'); ?></th>
-                <td><input type="text" name="<?php echo $this->plugin_name; ?>_custom_smtp_host" value="<?php echo esc_attr(get_option('<?php echo $this->plugin_name; ?>_custom_smtp_host')); ?>" /></td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"><?php esc_html_e('SMTP Port', 'wp-mailify'); ?></th>
-                <td><input type="text" name="<?php echo $this->plugin_name; ?>_custom_smtp_port" value="<?php echo esc_attr(get_option('<?php echo $this->plugin_name; ?>_custom_smtp_port')); ?>" /></td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"><?php esc_html_e('SMTP Username', 'wp-mailify'); ?></th>
-                <td><input type="text" name="<?php echo $this->plugin_name; ?>_custom_smtp_username" value="<?php echo esc_attr(get_option('<?php echo $this->plugin_name; ?>_custom_smtp_username')); ?>" /></td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"><?php esc_html_e('SMTP Password', 'wp-mailify'); ?></th>
-                <td><input type="password" name="<?php echo $this->plugin_name; ?>_custom_smtp_password" value="<?php echo esc_attr(get_option('<?php echo $this->plugin_name; ?>_custom_smtp_password')); ?>" /></td>
-            </tr>
-        </table>
-        <?php submit_button(); ?>
+        <?php
+        settings_fields( 'wp_mailify_option_group' );
+        do_settings_sections( 'wp-mailify-admin' );
+        submit_button();
+        ?>
     </form>
 </div>

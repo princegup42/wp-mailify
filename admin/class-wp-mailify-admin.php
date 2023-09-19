800c3f11ff8c9ff5001e532f6a496cfff6f5cfc7
princegup42 <guptasum521@gmail.com>
2023-09-19 22:00:32 +0530

    Define settings and callbacks in wp_mailify_page_init

--- admin/class-wp-mailify-admin.php
+++ admin/class-wp-mailify-admin.php
@@ -39,6 +39,7 @@ class Wp_Mailify_Admin {
 	 * @var      string    $version    The current version of this plugin.
 	 */
 	private $version;
+	private $wp_mailify_options;
 
 	/**
 	 * Initialize the class and set its properties.
@@ -99,6 +100,144 @@ class Wp_Mailify_Admin {
 	 */
 	public function wp_mailify_display_plugin_setup_page() {
 		include_once( 'partials/wp-mailify-admin-display.php' );
+	}
+
+	public function wp_mailify_page_init() {
+		register_setting(
+			'wp_mailify_option_group', // option_group
+			'wp_mailify_option_name', // option_name
+			array( $this, 'wp_mailify_sanitize' ) // sanitize_callback
+		);
+
+		add_settings_section(
+			'wp_mailify_setting_section', // id
+			'Settings', // title
+			array( $this, 'wp_mailify_section_info' ), // callback
+			'wp-mailify-admin' // page
+		);
+
+		add_settings_field(
+			'from_email_address', // id
+			'From Email Address', // title
+			array( $this, 'from_email_address_callback' ), // callback
+			'wp-mailify-admin', // page
+			'wp_mailify_setting_section' // section
+		);
+
+		add_settings_field(
+			'from_name', // id
+			'From Name', // title
+			array( $this, 'from_name_callback' ), // callback
+			'wp-mailify-admin', // page
+			'wp_mailify_setting_section' // section
+		);
+
+		add_settings_field(
+			'smtp_host', // id
+			'SMTP Host', // title
+			array( $this, 'smtp_host_callback' ), // callback
+			'wp-mailify-admin', // page
+			'wp_mailify_setting_section' // section
+		);
+
+		add_settings_field(
+			'smtp_port', // id
+			'SMTP Port', // title
+			array( $this, 'smtp_port_callback' ), // callback
+			'wp-mailify-admin', // page
+			'wp_mailify_setting_section' // section
+		);
+
+		add_settings_field(
+			'smtp_username', // id
+			'SMTP Username', // title
+			array( $this, 'smtp_username_callback' ), // callback
+			'wp-mailify-admin', // page
+			'wp_mailify_setting_section' // section
+		);
+
+		add_settings_field(
+			'smtp_password', // id
+			'SMTP Password', // title
+			array( $this, 'smtp_password_callback' ), // callback
+			'wp-mailify-admin', // page
+			'wp_mailify_setting_section' // section
+		);
+	}
+
+	public function wp_mailify_sanitize($input) {
+		$sanitary_values = array();
+		if ( isset( $input['from_email_address'] ) ) {
+			$sanitary_values['from_email_address'] = sanitize_text_field( $input['from_email_address'] );
+		}
+
+		if ( isset( $input['from_name'] ) ) {
+			$sanitary_values['from_name'] = sanitize_text_field( $input['from_name'] );
+		}
+
+		if ( isset( $input['smtp_host'] ) ) {
+			$sanitary_values['smtp_host'] = sanitize_text_field( $input['smtp_host'] );
+		}
+
+		if ( isset( $input['smtp_port'] ) ) {
+			$sanitary_values['smtp_port'] = sanitize_text_field( $input['smtp_port'] );
+		}
+
+		if ( isset( $input['smtp_username'] ) ) {
+			$sanitary_values['smtp_username'] = sanitize_text_field( $input['smtp_username'] );
+		}
+
+		if ( isset( $input['smtp_password'] ) ) {
+			$sanitary_values['smtp_password'] = sanitize_text_field( $input['smtp_password'] );
+		}
+
+		return $sanitary_values;
+	}
+
+	public function wp_mailify_section_info() {
+		echo '<p>' . esc_html__('Simplify your email sending process and enhance deliverability using WP Mailify\'s user-friendly interface', 'wp-mailify') . '</p>';
+	}
+
+	public function from_email_address_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="wp_mailify_option_name[from_email_address]" id="from_email_address" value="%s">',
+			isset( $this->wp_mailify_options['from_email_address'] ) ? esc_attr( $this->wp_mailify_options['from_email_address']) : ''
+		);
+	}
+
+	public function from_name_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="wp_mailify_option_name[from_name]" id="from_name" value="%s">',
+			isset( $this->wp_mailify_options['from_name'] ) ? esc_attr( $this->wp_mailify_options['from_name']) : ''
+		);
+	}
+
+	public function smtp_host_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="wp_mailify_option_name[smtp_host]" id="smtp_host" value="%s">',
+			isset( $this->wp_mailify_options['smtp_host'] ) ? esc_attr( $this->wp_mailify_options['smtp_host']) : ''
+		);
+	}
+
+	public function smtp_port_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="wp_mailify_option_name[smtp_port]" id="smtp_port" value="%s">',
+			isset( $this->wp_mailify_options['smtp_port'] ) ? esc_attr( $this->wp_mailify_options['smtp_port']) : ''
+		);
+	}
+
+	public function smtp_username_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="wp_mailify_option_name[smtp_username]" id="smtp_username" value="%s">',
+			isset( $this->wp_mailify_options['smtp_username'] ) ? esc_attr( $this->wp_mailify_options['smtp_username']) : ''
+		);
+	}
+
+	public function smtp_password_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="wp_mailify_option_name[smtp_password]" id="smtp_password" value="%s">',
+			isset( $this->wp_mailify_options['smtp_password'] ) ? esc_attr( $this->wp_mailify_options['smtp_password']) : ''
+		);
 	}
 
 	/**
